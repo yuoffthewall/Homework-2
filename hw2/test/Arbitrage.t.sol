bcf2b52565b053f24609a7b95c6c509917367e48
tommy <tommy8234@gmail.com>
2024-04-11 00:14:40 +0800

    Finish arbitrage test contract

--- hw2/test/Arbitrage.t.sol
+++ hw2/test/Arbitrage.t.sol
@@ -79,6 +79,16 @@ contract Arbitrage is Test {
         /**
          * Please add your solution below
          */
+
+        address[] memory path;
+        path = new address[](5);
+        path[0] = address(tokenB);
+        path[1] = address(tokenA);
+        path[2] = address(tokenD);
+        path[3] = address(tokenC);
+        path[4] = address(tokenB);
+        router.swapExactTokensForTokens(5 ether, 0, path, arbitrager, block.timestamp + 1000 * 5);
+
         /**
          * Please add your solution above
          */
